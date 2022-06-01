ac438d8c0737406289e93fb36ae38f22bacdbb92
Andy Wu <andywu0913@yahoo.com.tw>
2022-06-01 22:14:46 +0800

    Feature: Add initial express framework for gate client.

---
+++ gate-client/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
