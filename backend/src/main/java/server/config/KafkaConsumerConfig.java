c3139ea8b85823b2ee6c58c4ee760cd505c983cd
Andy Wu <andywu0913@yahoo.com.tw>
2022-06-12 19:05:03 +0800

    Feature: Change kafka connection encryption to SASL_SSL.

--- backend/src/main/java/server/config/KafkaConsumerConfig.java
+++ backend/src/main/java/server/config/KafkaConsumerConfig.java
@@ -3,7 +3,10 @@ package server.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -22,6 +25,21 @@ public class KafkaConsumerConfig {
   @Value("${spring.kafka.bootstrap-servers}")
   private String bootstrapServers;
 
+  @Value("${spring.kafka.properties.security.protocol}")
+  private String securityProtocal;
+
+  @Value("${spring.kafka.properties.sasl.mechanism}")
+  private String saslMechanism;
+
+  @Value("${spring.kafka.properties.sasl.jaas.config}")
+  private String saslJaasConfig;
+
+  @Value("${spring.kafka.consumer.ssl.trust-store-location}")
+  private String sslTrustStoreLocation;
+
+  @Value("${spring.kafka.consumer.ssl.trust-store-password}")
+  private String sslTrustStorePassword;
+
   @Value("${spring.kafka.consumer.group-id}")
   private String groupID;
 
@@ -31,7 +49,14 @@ public class KafkaConsumerConfig {
   @Bean
   public Map<String, Object> consumerConfigs() {
     Map<String, Object> props = new HashMap<>();
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    props.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocal);
+
+    props.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
+    props.put(SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig);
+    props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, sslTrustStoreLocation);
+    props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, sslTrustStorePassword);
+
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
     props.put(ConsumerConfig.AUTO_OFFSET_RESET_DOC, autoOffsetReset);
